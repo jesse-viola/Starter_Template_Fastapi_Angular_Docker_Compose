0240d90fad632bc2f1d74317de0db3ea7d9b1a43
mckenzie <mckenziness@gmail.com>
2025-08-10 09:24:12 -0700

    adding initial feature files

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,11 +8,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { FormsModule } from '@angular/forms';
+import { FavoritesComponent } from './features/favorites/favorites.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FavoritesComponent
   ],
   imports: [
     BrowserModule, HttpClientModule,
